bdc2c1e6c6a8ee3fc96ef0f6ca5341374d5ff639
Julio Cesar <julio.souza7@hotmail.com>
2022-07-01 18:40:00 -0300

    organization and javascript
    - Created a separate folder for images used on html files
    - Started an ideia on the script behind the login section

---
+++ script.js
@@ -0,0 +1,27 @@
+/*
+//----------------Usuários e Senhas----------------//
+
+let usuario = ["juliocsx", "ze123"]
+let senha = [123456, 234567]
+let nome = ["Julio", "José"]
+
+//-------------------------------------------------//
+
+
+let contador = 1
+while (contador = 1) {
+    for (var i = 0; i < usuario.length; i++) {
+        check = 0
+        if usuarioInput = usuario[check]
+            console.log ("Usuário correto")
+
+            contador = 0
+        else
+            check++
+    }
+}
+*/
+
+document.querySelector('botao').onclick = function() {
+    alert('Ai! Pare de me cutucar!');
+}
