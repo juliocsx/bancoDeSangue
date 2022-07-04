d1fc710bf2c40b05d8c8fe572ef82fec59acf5df
Julio Cesar <julio.souza7@hotmail.com>
2022-07-04 19:24:51 -0300

    script attempt
    Tried to make a login script, but it's not working the way it should.
    
    Also had help from friends which made my code cleaner and shorter.

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-/*
-//----------------Usuários e Senhas----------------//
+function login() {
 
-let usuario = ["juliocsx", "ze123"]
-let senha = [123456, 234567]
-let nome = ["Julio", "José"]
+    //----------Usuários e Senhas cadastrados----------//
 
-//-------------------------------------------------//
+    let usuarioC = ["juliocsx", "ze123"];
+    let senhaC = ["123", "456"];
+    //let nome = ["Julio", "José"];
+
+    //-------------------------------------------------//
+
+    // let contador = true;
+    // let usuarioInput = "juliocsx";//document.getElementById("usuario");
+    // let senhaInput = "123";//document.getElementById("senha");
+    let loginConf = false;
 
 
-let contador = 1
-while (contador = 1) {
-    for (var i = 0; i < usuario.length; i++) {
-        check = 0
-        if usuarioInput = usuario[check]
-            console.log ("Usuário correto")
+    // for (var i = 0; i < usuarioC.length; ++i) {
+    //     check = 0;
+    //     if ((usuarioInput = usuarioC[check]) && (senhaInput = senhaC[check])) {
+    //         loginConf = true;
+    //     } else {
+    //         check++;
+    //     }
+    // }
 
-            contador = 0
-        else
-            check++
+    if (loginConf = false) {
+        document.getElementById("login").style.display = "flex";
+    } else if (loginConf = true) {
+        window.location = "https://www.google.com/";
     }
-}
-*/
 
-document.querySelector('botao').onclick = function() {
-    alert('Ai! Pare de me cutucar!');
 }
+
+
